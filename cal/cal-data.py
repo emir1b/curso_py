59e265fbbdef5cbab2fb02d300ab3aafb1409aca
emir1b <emirbarnes5@gmail.com>
2025-05-16 21:03:18 +0000

    add files calculator

---
+++ cal/cal-data.py
@@ -0,0 +1,8 @@
+#variables
+num1="n1" 
+num2="n2"
+oper="op"
+result=evalt(f"{num1}{oper}{num2}")
+
+#resultado en pantalla 
+print(result) 
---
+++ cal/cal-input.py
@@ -0,0 +1,12 @@
+#variable
+num1="n1"=input()
+num2="n2"=input()
+oper="op"=input()
+result=evalt(f"{num1}{oper}{num2}")
+
+#resultado en pantalla 
+print(result) 
+
+
+
+evalt(f"{n1}{op}{n2}")
