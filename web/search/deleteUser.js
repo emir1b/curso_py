4671eca7df69af157adbea76dd5b4db8091aecef
emir1b <emirbarnes5@gmail.com>
2025-08-28 21:20:19 +0000

    delete user

--- web/search/deleteUser.js
+++ web/search/deleteUser.js
@@ -2,10 +2,20 @@ import { $, createtablet } from "./main.js";
 import data from "./data.js"
 const form =$ (".form-delete")
 
+
 form.addEventListener("submit",(ev) => {
     ev.preventDefault()
     const field = Object.fromEntries(new FormData(ev.target))
-
- data.push(field)
- createtablet()
+    console.log(field.name)
+    if (field.name !== " "|| field.age !== " ") {
+        data.forEach((element,index) => {
+            let name = element.name.toLowerCase()
+            let formname = field.name.toLowerCase()
+            if (name === formname && element.age === field.age) {
+                // el metodo esplice funcionan como una navaja zuiza para arrays. puede insertar remnover y reemplazar elementos
+                data.splice(index, 1)
+            }
+        })
+    }
+    createtablet()
 })
