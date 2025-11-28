dbd86f0e6ec4e8b4cf08da3d55270ed82d524b0f
emir1b <emirbarnes5@gmail.com>
2025-11-28 20:17:22 +0000

    juego

--- python/games/00_g/chararter.py
+++ python/games/00_g/chararter.py
@@ -1,28 +1,25 @@
-import pygame as pg 
-class Character(pg.sprite.Sprite):
-    def __init__(self,pos,image):
-        pg .sprite.Sprite.__init__(self)
-        self.image=image
-        self.rect =self.image.get_rect()
-        self.rect.center=pos
+import pygame as pg
+class Character(pg.sprite.Sprite): # Heredar sprite de pygame
+  def __init__(self,pos, imagen): #metodo constructor
+    pg.sprite.Sprite.__init__(self) #Image aspector 
+    self.image = imagen
+    self.rect = self.image.get_rect()
+    self.rect.center = pos
+
+  #metodo para mover a la derecha
+  def move_right(self,new_pos): # recibir la nueva posicion(new_pos)
+    #actualizar posicion
+    self.rect.x += new_pos
     
-    def move_x(self,new_pos):
-        return self.rect.x 
-
-        
-    
-def character(pg, screen, dt):
-    #pocición del jugador
-    player_pos = pg.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-    pg.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pg.key.get_pressed()
-    if keys[pg.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pg.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pg.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pg.K_d]:
-        player_pos.x += 300 * dt
+  #metodo para mover a la derecha
+  def move_left(self,new_pos): # recibir la nueva posicion(new_pos)
+    #actualizar posicion
+    self.rect.x -= new_pos
+  #metodo para mover a la derecha
+  def move_up(self,new_pos): # recibir la nueva posicion(new_pos)
+    #actualizar posicion
+    self.rect.y -= new_pos
+  #metodo para mover a la derecha
+  def move_down(self,new_pos): # recibir la nueva posicion(new_pos)
+    #actualizar posicion
+    self.rect.y += new_pos
--- python/games/00_g/enemy.py
+++ python/games/00_g/enemy.py
@@ -1,21 +1,16 @@
-import pygame as pg 
-#crear una plantilla enemiga 
-class Enemies(pg.sprite.Sprite) :
-    def __init__(self,pos,image):#method
-        pg.sprite.Sprite.__init__(self) #heredar las imagenes de py game
-        self.image=image
-        self.rect = self.image.get_rect()
-        self.rect.center = pos
+import pygame as pg
+#crear una plantilla enemiga
+class Enemies (pg.sprite.Sprite)  :
+  def __init__(self,pos,image) -> None:
+    pg.sprite.Sprite.__init__(self) #Image aspector 
+    self.image= image
+    self.rect = self.image.get_rect()
+    self.rect.center = pos 
 
-    def update(self):
-        self.move()
-
-    def move(self):
-       self.rect.x += 1 
-
-
-      
+  def upgrade(self,new_pos=None):
+    self.move(new_pos)
+  
+  def move(self,new_pos=None):
+    self.rect.x += new_pos
     
-    
-
 
